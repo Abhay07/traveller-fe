2e9c2cf07b776a39214baed498b654ecfa9db1e7
abhay-srivastav <abhaysrivastav007@gmail.com>
2019-05-26 11:12:36 +0530

    Updates

---
+++ precache-manifest.cfad1919c8aa531e9d877ece73800083.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2802ba072ee5b658df93166b5b64879a",
+    "url": "/traveller-fe/index.html"
+  },
+  {
+    "revision": "11c749e478adf5690d18",
+    "url": "/traveller-fe/static/css/main.c88a48b7.chunk.css"
+  },
+  {
+    "revision": "4f82d731621a53bdeecb",
+    "url": "/traveller-fe/static/js/2.fe8d3990.chunk.js"
+  },
+  {
+    "revision": "11c749e478adf5690d18",
+    "url": "/traveller-fe/static/js/main.0d8061ed.chunk.js"
+  },
+  {
+    "revision": "029f1ccf83d0905c9003",
+    "url": "/traveller-fe/static/js/runtime~main.e7a31cd2.js"
+  }
+]);
